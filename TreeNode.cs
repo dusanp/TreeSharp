ff40f86229e77e7764f6d32fe43dccb32fd56bed
dusanp <dusan.pilka@gmail.com>
2017-03-07 13:55:23 +0100

    Version 1.1
    Improved behavior when nodes dont have a value

--- TreeNode.cs
+++ TreeNode.cs
@@ -11,26 +11,39 @@ namespace TreeSharp
         private static readonly string[] Separators = new[] {"\r\n", "\n"};
         public abstract TCollection ChildNodes { get; set; }
 
+        /// <summary>
+        /// Returns a string representation of the tree to which this node is root.
+        /// </summary>
+        /// <param name="indentLevel">Amount of characters by which level in the tree is indented. </param>
+        /// <param name="maxDepth">Maximum tree depth printed.</param>
+        /// <returns></returns>
         public string GetTree(byte indentLevel = 3, byte maxDepth = Byte.MaxValue)
         {
             var sb = new StringBuilder(ToString());
-            sb.AppendLine();
-            if (ChildNodes == null|| maxDepth < 1)
+            var isInline = ToString() == "";
+            if (!isInline) {sb.AppendLine();}
+            if (ChildNodes == null || !ChildNodes.Any() || maxDepth < 1)
             {
                 return sb.ToString();
             }
             var last = ChildNodes.Last();
+            var first = ChildNodes.First();
             foreach (var childNode in ChildNodes)
             {
-                sb.Append(IndentChild(childNode.GetTree(indentLevel, (byte) (maxDepth-1)), Object.ReferenceEquals(childNode, last), indentLevel));
+                sb.Append(IndentChild(childNode.GetTree(
+                    indentLevel, 
+                    (byte) (maxDepth-1)),
+                    Object.ReferenceEquals(childNode, last), 
+                    isInline && Object.ReferenceEquals(childNode, first), 
+                    indentLevel));
             }
             return sb.ToString();
         }
 
-        private static string IndentChild(string child, bool isLast, byte indentLevel)
+        private static string IndentChild(string child, bool isLast, bool isInline, byte indentLevel)
         {
             var lines = child.Split(Separators, Options);
-            var sb = new StringBuilder(isLast?"└": "├");
+            var sb = new StringBuilder(isInline ? (isLast ? "─" : "┬") : (isLast ? "└" : "├"));
             sb.Append(new string('─', indentLevel));
             sb.AppendLine(lines.First());
             var indentBlock = (isLast ? " " : "│")+new string(' ', indentLevel);
@@ -42,6 +55,11 @@ namespace TreeSharp
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Prints a string representation of the tree to which this node is root to console.
+        /// </summary>
+        /// <param name="indentLevel">Amount of characters by which level in the tree is indented. </param>
+        /// <param name="maxDepth">Maximum tree depth printed.</param>
         public void PrintTree(byte indentLevel = 3, byte maxDepth = Byte.MaxValue)
         {
             Console.WriteLine(GetTree(indentLevel, maxDepth));
